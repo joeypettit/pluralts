4fc9cf0ae1844bf51baef92b74f3342b45e6e94e
Joey Pettit <pettitbass@gmail.com>
2023-05-09 21:27:32 -0500

    first commit

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+function startGame(){
+    const messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+document.getElementById('startGame').addEventListener('click', startGame);
